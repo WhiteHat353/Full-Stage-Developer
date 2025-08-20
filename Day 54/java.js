84768f0adaa5e2c2d65be491960fbdf3e2ffb4e1
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-08-20 22:46:59 +0500

    Introduction to JavaScript & Installing Node.js
    Today I learn Introduction to JavaScript and Installing Node.js.JS stand for (JavaScript) its is Client Side Scripting. We run JS 2 ways 1) Frond End. 2) Back End. Node.js is Chrome v8 Engine which are use to run our scripts and provide local server to run its scripts on local servers. And we also download  "npm" which stand for "Node Packet Manager"  to download other packets and reuse it in local servers.

---
+++ Day 54/java.js
@@ -0,0 +1,19 @@
+alert("Welcome to Sigma Web Development Course.")
+
+console.log("Code is runining...")
+console.log("Code is  also runining...")
+console.log("Code is looking like a wow ...")
+
+var a = prompt("Enter a Your Number : ")
+
+var isTrue= confirm("Are you Sure to leave the Page")
+if(isTrue){
+    console.log("You Computer is Blast!")
+}
+else{
+    console.log("You Computer is not Blast!")
+}
+
+console.log("Your Number is " + a )
+
+document.title = "I am Good."
---
+++ Day 54/n_java.js
@@ -0,0 +1,5 @@
+console.log("Hello World.")
+console.log("Code is runining...")
+console.log("Code is  also runining...")
+console.log("Code is looking like a wow ...")
+
