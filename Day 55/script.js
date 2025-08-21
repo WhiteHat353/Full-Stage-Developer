85d09eb83e101662ba059b4d98c9359be96d5abf
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-08-21 22:09:56 +0500

    JavaScript Variables, Datatype and Objects
    Today I learn what is Variables, Rules of variables name. In rules , I see difference b/w let, var and const. I also learn about Data type and its types, like primitives and objects.

---
+++ Day 55/script.js
@@ -0,0 +1,35 @@
+alert("Hello World");
+
+console.log("Hello today is very hot day....");
+// Variables
+var a = confirm("Heloo babys.")
+var num1 = 89;
+var num2 = 111;
+var c = "Abdul Jabbar";
+
+console.log(num1 + num2);
+
+// // DataType
+console.log(typeof num1, typeof num2, typeof c);
+// constent
+const a1 = 5;
+a1 + 1 ;
+console.log(a);
+// let
+let n1 = 5;
+let n2 = 6;
+{
+    let n1 = 56;
+    console.log(n1);
+
+}
+console.log(n1);
+
+// Object
+let obj = {
+    name : "Abdul Jabbar",
+    "Job Code": 5660
+}
+console.log(obj);
+obj.salary = "10000cr";
+console.log(obj);
