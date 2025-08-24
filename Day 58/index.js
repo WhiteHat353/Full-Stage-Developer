717034eaec7f9e5e866949208a663daa18bd15f5
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-08-24 21:47:27 +0500

    JavaScript Functions
    Today I learn Functions is like a block of code which receive parameters and return single value. I see What is parameters or attributes and how to pass it. I see default parameters and its value. I also see arrow function.

---
+++ Day 58/index.js
@@ -0,0 +1,26 @@
+// console.log("Hey AbdulJabbar you are good.");
+// console.log("Hey AbdulJabbar you are nice.");
+// console.log("Hey AbdulJabbar you t-shirt is also good.");
+// console.log("Hey AbdulJabbar you course is good.");
+
+// functions
+// function nice(name) {
+//     console.log("Hey " + name + " you are good.");
+//     console.log("Hey " + name + "you are nice.");
+// }
+// nice("AbdulJabbar");
+
+// function of sum
+// c is default value
+// function sum(a,b, c =34){
+//     // console.log(a+b);
+//     return a + b + c;
+// }
+// result = sum (5,15);
+// console.log("The Sum of these two values is :" + result)
+
+// arrow Function 
+const name = (a, b)=>{
+    console.log (a+b);
+}
+name(5 ,6);
