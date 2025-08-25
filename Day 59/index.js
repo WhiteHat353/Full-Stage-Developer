cba68497fda167cb844f6c44521d92c3d802eee8
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-08-25 21:32:32 +0500

    Exercise 9 - Faulty Calculator
    Today Sir give me task to create "Faulty Calculator" which performs wrong operations. Performs wrong operations details in inde.html file. Go to this file and read it and create faulty calculator.

---
+++ Day 59/index.js
@@ -0,0 +1,11 @@
+/* Create a faulty calculator
+
+This faulty calculator does following:
+1. It takes 2 number as input from the user.
+2. It perform wrong operations as follow:
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+It performs wrong operation  10%  of the Time.
+*/
