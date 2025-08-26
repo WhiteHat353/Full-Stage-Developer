b4e9ed82f9f1b2bcb2af86b6963d5f6a2ecce2ee
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-08-26 21:41:44 +0500

    String in javaScript
    Today I learn about "String". String is collections of characters. Also see with  functions like length, uppercase, lowercase, startwith, endwith,and slice of string and many more.

---
+++ Day 60/index.js
@@ -0,0 +1,36 @@
+console.log("This is a String tutorinl");
+let n = " Abdul Jabbar";
+console.log(n);
+
+console.log(n[0]);
+console.log(n[1]);
+console.log(n[2]);
+console.log(n[3]);
+console.log(n[4]);
+console.log(n[5]);
+console.log(n[6]);
+console.log(n[7]);
+console.log(n[8]);
+console.log(n[9]);
+console.log(n[10]);
+
+// length of arrny
+console.log(n.length);
+
+// Upper case
+let b = "abdul jabbar";
+
+console.log(b.toUpperCase());
+
+let c = "noneofthebusiness";
+// Slicing of String
+console.log(c.slice(0,9))
+console.log(c.slice(1))
+console.log(c.slice(9))
+
+// replace the content
+
+let d = "Bhai";
+console.log(d.replace("Bhai","Bha"));
+
+console.log(d.startsWith("Bh"));
