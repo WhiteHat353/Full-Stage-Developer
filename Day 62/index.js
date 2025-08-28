26151e5c9b0a461ea96982c300982cb674ab89db
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-08-28 21:26:07 +0500

    Business Name Generator
    Today Sir give me task to create a program which generate random business name.

---
+++ Day 62/index.js
@@ -0,0 +1,19 @@
+/*Create a business name generator by combining list of adjectives and shop name and another name 
+
+Adjectives:
+Crazy
+Amazon
+Fire
+
+Shop Name:
+Foods
+Engine
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+*/
+
+// Output: Crazy Foods Hub 
