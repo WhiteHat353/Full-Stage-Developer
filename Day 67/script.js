05b88e58abff1fae455f82b606f5c0ae2947d8c2
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-09-02 17:27:01 +0500

    DOM - Children, Parent & Sibling Nodes
    Today I learn about DOM and its functions.

---
+++ Day 67/script.js
@@ -0,0 +1,27 @@
+console.log("Hello World");
+
+document.body
+
+document.body.childNodes
+
+// Hello World
+// document.body.childNodes[1].childNodes
+// [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// let const = document.body.childNodes[1]
+// let cons = document.body.childNodes[1]
+// undefined
+// cons.childNodes
+// [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// cons.last
+// undefined
+
+// Hello World
+// cons
+// document.body.childNodes[1].childNodes
+// [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// let cons = document.body.childNodes[1]
+// undefined
+// cons.childNodes
+// [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+// cons.firstElementChild
+// <div class=​"box">​Box1​</div>​
