86ce03ce2ca80e95ad975fb8f2d914281c171c03
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-09-07 20:33:13 +0500

    Exercise 12 - Solution
    Today I solve  Exercise 12 and see how to  generate random color.

---
+++ Day 72/script.js
@@ -0,0 +1,19 @@
+console.log("I am runining...");
+
+// let boxes = document.getElementsByClassName("box");
+let boxes = document.querySelector(".container").children;
+console.log(boxes);
+
+
+function getRandomColor(){
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+
+
+Array.from(boxes).forEach(e=>{
+    e.style.backgroundcolor = getRandomColor();
+    e.style.color = getRandomColor();
+})
