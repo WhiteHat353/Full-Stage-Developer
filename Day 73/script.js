bd03d46a7b4d6ab641f68abbf2914df554e38bb8
Abduljabbar53 <abduljaabbar012@gmail.com>
2025-09-08 21:00:48 +0500

    Exercise 13 - Dynamic Website Builder
    Today Sir give task to build a dynamic website.

---
+++ Day 73/script.js
@@ -0,0 +1,4 @@
+function createCard(title,cName, views,monthsOld, duration,thumbnail){
+// finish this function 
+}
+createdCard("Sigma Web Dev no 2", "CodeWithHarry", 450000, 7 , "35.45", "https://i.ytimg.com/vi/kJEsTjH5mVg/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLBTa4i6NuryzZhcPo7BkCK1G6PqGg")
